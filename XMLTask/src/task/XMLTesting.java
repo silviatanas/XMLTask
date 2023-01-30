fbc60d50fbdf017ace92df31bf4bf1b8a842d4b8
silviatanas <93332302+silviatanas@users.noreply.github.com>
2023-01-30 17:23:20 +0200

    Update XMLTesting.java

--- XMLTask/src/task/XMLTesting.java
+++ XMLTask/src/task/XMLTesting.java
@@ -1,7 +1,6 @@
 package task;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
@@ -17,31 +16,47 @@ public class XMLTesting {
             DocumentBuilder builder = factory.newDocumentBuilder();
             Document doc = builder.parse(xmlContent);
 
+            // Getting all units
             NodeList unitList = doc.getElementsByTagName("unit");
 
             for (int i = 0; i < unitList.getLength(); i++) {
                 Node unitNode = unitList.item(i);
 
+                // Getting units with specified id
                 if (unitNode.getAttributes().getNamedItem("id")
                         .getNodeValue().equals("org.eclipse.equinox.p2.director")) {
                     NodeList childrenList = unitNode.getChildNodes();
 
+                    System.out.println(unitNode.getAttributes().getNamedItem("id").getNodeValue());
+
+                    // Getting all children nodes of the unit with that id
                     for (int j = 0; j < childrenList.getLength(); j++) {
                         Node child = childrenList.item(j);
 
+                        // Getting requires
                         if (child.getNodeName().equals("requires")) {
                             NodeList requiresList = child.getChildNodes();
 
+                            // This contains all the required nodes
                             for (int k = 0; k < requiresList.getLength(); k++) {
                                 Node requiredNode = requiresList.item(k);
 
-                                if (requiredNode.getAttributes().getNamedItem("namespace")
-                                        .getNodeValue().equals("org.eclipse.equinox.p2.iu") ||
-                                        requiredNode.getAttributes().getNamedItem("namespace")
-                                                .getNodeValue().equals("osgi.bundle")) {
-                                    if (!requiredNode.getAttributes().getNamedItem("name")
-                                            .getNodeValue().equals("org.eclipse.equinox.p2.director")) {
+                                // Node includes a text and an element node,
+                                // in order to avoid the null value of the text we filter
+                                if (requiredNode.getNodeType() == Node.ELEMENT_NODE) {
+                                    if (requiredNode.getAttributes().getNamedItem("namespace")
+                                            .getNodeValue().equals("org.eclipse.equinox.p2.iu") ||
+                                            requiredNode.getAttributes().getNamedItem("namespace")
+                                                    .getNodeValue().equals("osgi.bundle")) {
 
+                                        System.out.print("--");
+                                        System.out.println(requiredNode.getAttributes().getNamedItem("name").getNodeValue());
+
+                                        if (!requiredNode.getAttributes().getNamedItem("name")
+                                                .getNodeValue().equals("org.eclipse.equinox.p2.director")) {
+                                            System.out.print("--");
+                                            System.out.println(requiredNode.getAttributes().getNamedItem("name").getNodeValue());
+                                        }
                                     }
                                 }
                             }
