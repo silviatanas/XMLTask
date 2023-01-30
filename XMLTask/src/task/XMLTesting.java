b7b394b06f1bb4d95b59135b0d1a85f299e5f6e0
silviatanas <93332302+silviatanas@users.noreply.github.com>
2023-01-30 23:59:43 +0200

    Update XMLTesting.java

--- XMLTask/src/task/XMLTesting.java
+++ XMLTask/src/task/XMLTesting.java
@@ -3,11 +3,11 @@ package task;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import java.util.Scanner;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.*;
+import java.util.Scanner;
 
 public class XMLTesting {
     public static void main(String[] args) {
@@ -17,54 +17,60 @@ public class XMLTesting {
             DocumentBuilder builder = factory.newDocumentBuilder();
             Document doc = builder.parse(xmlContent);
             Scanner sc = new Scanner(System.in);
-            
+
             // User input to search id
-            System.out.println("Enter unit id:");
-            String input = sc.nextLine();
+            for (boolean flag = false; flag == false; ) {
+                System.out.println("Enter unit id:");
+                String input = sc.nextLine();
 
-            // Getting all units
-            NodeList unitList = doc.getElementsByTagName("unit");
+                // Getting all units
+                NodeList unitList = doc.getElementsByTagName("unit");
 
-            for (int i = 0; i < unitList.getLength(); i++) {
-                Node unitNode = unitList.item(i);
+                for (int i = 0; i < unitList.getLength(); i++) {
+                    Node unitNode = unitList.item(i);
 
-                // Getting units with specified id
-                if (unitNode.getAttributes().getNamedItem("id")
-                        .getNodeValue().equals(input)) {
-                    NodeList childrenList = unitNode.getChildNodes();
+                    // Getting units with specified id
+                    if (unitNode.getAttributes().getNamedItem("id")
+                            .getNodeValue().equals(input)) {
+                        flag = true;
+                        NodeList childrenList = unitNode.getChildNodes();
 
-                    System.out.println(unitNode.getAttributes().getNamedItem("id").getNodeValue());
+                        System.out.println(unitNode.getAttributes().getNamedItem("id").getNodeValue());
 
-                    // Getting all children nodes of the unit with that id
-                    for (int j = 0; j < childrenList.getLength(); j++) {
-                        Node child = childrenList.item(j);
+                        // Getting all children nodes of the unit with that id
+                        for (int j = 0; j < childrenList.getLength(); j++) {
+                            Node child = childrenList.item(j);
 
-                        // Getting requires
-                        if (child.getNodeName().equals("requires")) {
-                            NodeList requiresList = child.getChildNodes();
+                            // Getting requires
+                            if (child.getNodeName().equals("requires")) {
+                                NodeList requiresList = child.getChildNodes();
 
-                            // This contains all the required nodes
-                            for (int k = 0; k < requiresList.getLength(); k++) {
-                                Node requiredNode = requiresList.item(k);
+                                // This contains all the required nodes
+                                for (int k = 0; k < requiresList.getLength(); k++) {
+                                    Node requiredNode = requiresList.item(k);
 
-                                // Node includes a text and an element node,
-                                // in order to avoid the null value of the text we filter
-                                if (requiredNode.getNodeType() == Node.ELEMENT_NODE) {
-                                    if (requiredNode.getAttributes().getNamedItem("namespace")
-                                            .getNodeValue().equals("org.eclipse.equinox.p2.iu") ||
-                                            requiredNode.getAttributes().getNamedItem("namespace")
-                                                    .getNodeValue().equals("osgi.bundle")) {
+                                    // Node includes a text and an element node,
+                                    // in order to avoid the null value of the text we filter
+                                    if (requiredNode.getNodeType() == Node.ELEMENT_NODE) {
+                                        if (requiredNode.getAttributes().getNamedItem("namespace")
+                                                .getNodeValue().equals("org.eclipse.equinox.p2.iu") ||
+                                                requiredNode.getAttributes().getNamedItem("namespace")
+                                                        .getNodeValue().equals("osgi.bundle")) {
 
-                                        if (!requiredNode.getAttributes().getNamedItem("name")
-                                                .getNodeValue().equals(input)) {
-                                            System.out.print("--");
-                                            System.out.println(requiredNode.getAttributes().getNamedItem("name").getNodeValue());
+                                            if (!requiredNode.getAttributes().getNamedItem("name")
+                                                    .getNodeValue().equals(input)) {
+                                                System.out.print("--");
+                                                System.out.println(requiredNode.getAttributes().getNamedItem("name").getNodeValue());
+                                            }
                                         }
                                     }
                                 }
                             }
                         }
                     }
+                }
+                if (flag == false) {
+                    System.out.println("Id not found");
                 }
             }
 
